72493a184c2ca36333a288793c4c9c9e33cbabb1
Stas <stanislav.s.hlushko@gmail.com>
2024-01-25 22:49:52 +0200

    Change slider component

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,9 +1,8 @@
 import { FC, useEffect, useState } from 'react'
-import { IoMdArrowRoundBack, IoMdArrowRoundForward } from 'react-icons/io'
 
-import s from './Slider.module.scss'
+import { SliderComponent } from './SliderComponent'
 
-interface IListEl {
+export interface IListEl {
 	id: number
 	url: string
 	isActive: boolean
@@ -18,7 +17,7 @@ export const Slider: FC<IProps> = ({ images }) => {
 		images?.map((el, id) => ({ id: id + 1, url: el, isActive: id === 0 })),
 	)
 
-	const [currentImg, setCurrentImg] = useState(
+	const [currentImg, setCurrentImg] = useState<IListEl | undefined>(
 		imageList?.find(el => el.isActive),
 	)
 
@@ -43,8 +42,8 @@ export const Slider: FC<IProps> = ({ images }) => {
 	console.log(imageList)
 
 	const nextImg = () => {
-		const length = () => imageList ? imageList.length : 0
-		
+		const length = () => (imageList ? imageList.length : 0)
+
 		if (currentImg && currentImg?.id < length()) {
 			currentImg && changeCurrentImg(currentImg.id + 1)
 		}
@@ -60,34 +59,12 @@ export const Slider: FC<IProps> = ({ images }) => {
 	}, [imageList])
 
 	return (
-		<div className={s.slider}>
-			<div className={s.preview}>
-				<button className={s.prevBtn + ' ' + s.arrow} onClick={prevImg}>
-					<IoMdArrowRoundBack />
-				</button>
-				<div style={{ backgroundImage: `url(${currentImg?.url})` }}></div>
-				<button className={s.nextBtn + ' ' + s.arrow} onClick={nextImg}>
-					<IoMdArrowRoundForward />
-				</button>
-			</div>
-			<ul className={s.list}>
-				{imageList
-					? imageList.map(el => {
-							return (
-								<li
-									key={el.id}
-									onClick={() => changeCurrentImg(el.id)}
-									className={s.item}>
-									<img
-										src={el.url}
-										alt='img'
-										className={el.isActive ? s.active : ''}
-									/>
-								</li>
-							)
-					  })
-					: null}
-			</ul>
-		</div>
+		<SliderComponent
+			imageList={imageList}
+			currentImg={currentImg}
+			changeCurrentImg={changeCurrentImg}
+			prevImg={prevImg}
+			nextImg={nextImg}
+		/>
 	)
 }
---
+++ src/components/Slider/SliderComponent.tsx
@@ -0,0 +1,53 @@
+import { FC } from 'react'
+import { IoMdArrowRoundBack, IoMdArrowRoundForward } from 'react-icons/io'
+
+import s from './Slider.module.scss'
+import { IListEl } from './Slider'
+
+interface IProps {
+	prevImg: () => void
+	nextImg: () => void
+	changeCurrentImg: (id: number) => void
+	imageList: IListEl[] | undefined
+	currentImg: IListEl | undefined
+}
+
+export const SliderComponent: FC<IProps> = ({
+	imageList,
+	currentImg,
+	changeCurrentImg,
+	prevImg,
+	nextImg,
+}) => {
+	return (
+		<div className={s.slider}>
+			<div className={s.preview}>
+				<button className={s.prevBtn + ' ' + s.arrow} onClick={prevImg}>
+					<IoMdArrowRoundBack />
+				</button>
+				<div style={{ backgroundImage: `url(${currentImg?.url})` }}></div>
+				<button className={s.nextBtn + ' ' + s.arrow} onClick={nextImg}>
+					<IoMdArrowRoundForward />
+				</button>
+			</div>
+			<ul className={s.list}>
+				{imageList
+					? imageList.map(el => {
+							return (
+								<li
+									key={el.id}
+									onClick={() => changeCurrentImg(el.id)}
+									className={s.item}>
+									<img
+										src={el.url}
+										alt='img'
+										className={el.isActive ? s.active : ''}
+									/>
+								</li>
+							)
+					  })
+					: null}
+			</ul>
+		</div>
+	)
+}
